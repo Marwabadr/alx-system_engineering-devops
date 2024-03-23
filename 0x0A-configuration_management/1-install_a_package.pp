9cfcf927233aded2543cf43a6a88dce6eb49c716
Marwabadr <elbadrmarwa2002@gmail.com>
2024-03-23 09:19:08 -0700

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Install Flask using pip3
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  command  => '/usr/local/bin/pip3', # Assuming pip3 is installed at this location
+  require  => Package['python3-pip'], # Ensure python3-pip is installed first
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
